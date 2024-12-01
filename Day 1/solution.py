ad176c2a782e6af4a1a28118cf4e91b865874d27
hotaru <hotaru.py@gmail.com>
2024-12-02 01:32:16 +0530

    different ways to iterate through

--- Day 1/solution.py
+++ Day 1/solution.py
@@ -4,5 +4,5 @@ for i in open("input.txt").readlines():
   y.append(int(i.split()[1]))
 x.sort()
 y.sort()
-print(f"Task 1: {sum([abs(x[i]-y[i]) for i in range(len(x))])}")
-print(f"Task 2: {sum([x[i]*y.count(x[i]) for i in range(len(x))])}")
+print(f"Task 1: {sum([abs(a-b) for a,b in zip(x,y)])}")
+print(f"Task 2: {sum([i*y.count(i) for i in x])}")
